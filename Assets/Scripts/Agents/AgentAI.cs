39a7979d9947ac016a96d61596f800ee330fa709
KristynaPavlatova <krpavlatova@gmail.com>
2021-06-11 06:28:24 +0200

    done

--- Assets/Scripts/Agents/AgentAI.cs
+++ Assets/Scripts/Agents/AgentAI.cs
@@ -5,119 +5,125 @@ using UnityEngine;
 
 public class AgentAI : MonoBehaviour
 {
-    public bool _rayDebug = false;
-    public bool _availableDirCountDebug = true;
-    //public bool _moveDirDebug = false;
-    [HideInInspector]
-    public Vector3 lastCrossroadPosition;
-    public int lastCrossroadDirectionTaken;
-    [HideInInspector]
-    public List<int> pathTaken;
+    [Space(5)]
+    public bool rayDebug = false;
+    public bool availableDirDebug = false;
+    public bool directionDebug = false;
+    public bool genPathDebug = false;
     [HideInInspector]
     public bool isInDeadEnd = false;
+    [HideInInspector]
+    public bool shouldInitNewAgent = true;
 
-    private List<int> _currentlyAvailableDirections = new List<int>();
+    [HideInInspector]
+    public Vector3 lastCrossroadPosition;
+    [HideInInspector]
+    public List<int> pathHistory;
+    [HideInInspector]
+    public List<int> _genPath;
+    
     private int _numOfStepsTaken = 0;
-    private int _lastDirectionTaken = -1;
-    public List<int> genPath;
+    private int _lastDirectionTaken = -1;//deciding where you can move
+    private List<int> _currentlyAvailableDirections = new List<int>();
 
+    static System.Random rnd = new System.Random();
     private RaySidesAndDown _raycastScript;
     private AgentManager _agentManagerScript;
     private GameObject _agentManagerObj;
     private MoveDirection _moveScript;
-    static System.Random random = new System.Random();
 
     private void OnValidate()
     {
         _raycastScript = this.GetComponent<RaySidesAndDown>();
-                Debug.Assert(_raycastScript, "Agent does not have a RaySidesAndDown script!");
+        Debug.Assert(_raycastScript, "Agent does not have a RaySidesAndDown script!");
         _moveScript = this.GetComponent<MoveDirection>();
-                Debug.Assert(_moveScript, "Agent does not have a MoveDirection script!");
+        Debug.Assert(_moveScript, "Agent does not have a MoveDirection script!");
         _agentManagerObj = GameObject.FindGameObjectWithTag("agentManager");
-             Debug.Assert(_agentManagerObj, "AgentManager object not found!");
-        _agentManagerScript = GameObject.FindGameObjectWithTag("agentManager").GetComponent<AgentManager>();
-                Debug.Assert(_agentManagerScript, "AgentManager script not found!");
+        Debug.Assert(_agentManagerObj, "AgentManager object not found!");
+        _agentManagerScript = _agentManagerObj.GetComponent<AgentManager>();
+        Debug.Assert(_agentManagerScript, "AgentManager script not found!");
     }
-    private void Awake()
+    private void Start()
     {
-        string message = "GEN PATH in " + genPath.Count + " steps! Steps taken: ";
-        
-        foreach (int dir in genPath)
+        initAgent();
+
+        if (genPathDebug)
         {
-            message += dir + ", ";
+            string message = "GEN PATH in " + _genPath.Count + " steps! Steps taken: ";
+
+            foreach (int dir in _genPath)
+            {
+                message += dir + ", ";
+            }
+            Debug.Log(message);
         }
-        Debug.Log(message);
     }
     private void Update()
     {
-        if (!_raycastScript.StandingOnGoal())
+        initAgent();
+
+        if (!this._raycastScript.StandingOnGoal())
         {
-            if (!isInDeadEnd)
+            if (!this.isInDeadEnd)
             {
-                if (Input.GetKeyDown(KeyCode.Space))
+                UpdateAvailableDirections();
+                if (_agentManagerScript.stepByStep)
                 {
-                    UpdateAvailableDirections();
-                    if (_currentlyAvailableDirections.Count > 1)
-                    {
-                        lastCrossroadPosition = this.transform.position;//note before you move from the crossroad
-
-                        //Chose random:
-                        int dirTaken = UnityEngine.Random.Range(0, _currentlyAvailableDirections.Count);
-                        //Look what is the direction in generationPath at this currentStep + 1 --> because you move, then note that you made a step, so you have to look ahead 1 step than this agent's current number of steps
-                        if(_agentManagerScript.generationPath != null && genPath.Count >= _numOfStepsTaken)// && _agentManager.generationPath.Count >= (_numOfStepsTaken + 1)
-                        {
-                            //choose path based on probability:
-                            if (ShouldFollowGenerationPathStepWithProbability(_agentManagerScript.agentProbabilityToFollowGenPath))
-                            {
-                                Debug.Log("gen path at step"+_numOfStepsTaken+ " is " + genPath[_numOfStepsTaken]);
-                                dirTaken = genPath[_numOfStepsTaken];//take the same direction as in the previous generation
-
-                                _moveScript.MoveInDirection(dirTaken);
-                                _lastDirectionTaken = dirTaken;
-                                Debug.Log("More directions to go GenerationPath: " + dirTaken);
-                            }
-                            else
-                            {
-                                Debug.Log("1 More directions to go Random: " + _currentlyAvailableDirections[dirTaken]);
-                                _moveScript.MoveInDirection(_currentlyAvailableDirections[dirTaken]);
-                                _lastDirectionTaken = _currentlyAvailableDirections[dirTaken];
-                            }
-                        }
-                        else
-                        {
-                            Debug.Log("2 More directions to go Random: " + _currentlyAvailableDirections[dirTaken]);
-                            //just choose randomly:
-                            _moveScript.MoveInDirection(_currentlyAvailableDirections[dirTaken]);
-                            _lastDirectionTaken = _currentlyAvailableDirections[dirTaken];
-                        }
-                                                
-                        
-                        _numOfStepsTaken++;
-                        Debug.Log("Steps taken = " + _numOfStepsTaken);
-                    }
-                    else if (_currentlyAvailableDirections.Count == 1)
-                    {
-                        Debug.Log("Just one direction to go: " + _currentlyAvailableDirections[0]);
-
-                        //Just take the first availbale direction in the list
-                        _moveScript.MoveInDirection(_currentlyAvailableDirections[0]);
-                        _lastDirectionTaken = _currentlyAvailableDirections[0];
-                        pathTaken.Add(_currentlyAvailableDirections[0]);
-                        _numOfStepsTaken++;
-                        Debug.Log("Steps taken = " + _numOfStepsTaken);
-                    }
-                    else if (_currentlyAvailableDirections.Count <= 0)
-                    {
-                        Debug.Log("No direction to go");
-                        isInDeadEnd = true;
-                        _agentManagerScript.AgentAtDeadEnd(lastCrossroadPosition, _numOfStepsTaken, pathTaken);
-                    }
+                    if (Input.GetKeyDown(KeyCode.Space)) aiAlgorithm();
+                }
+                else
+                {
+                    aiAlgorithm();
                 }
             }
         }
         else
         {
-            _agentManagerScript.GoalFound(pathTaken);
+            _agentManagerScript.GoalFound(pathHistory);
+        }
+    }
+    private void aiAlgorithm()
+    {
+        if (_currentlyAvailableDirections.Count > 1)
+        {
+            lastCrossroadPosition = this.transform.position;//note before you move from the crossroad
+
+            //Chose random:
+            int dirTaken = UnityEngine.Random.Range(0, _currentlyAvailableDirections.Count);
+            //Look what is the direction in generationPath at this currentStep + 1 --> because you move, then note that you made a step, so you have to look ahead 1 step than this agent's current number of steps
+            if (_genPath != null && _genPath.Count >= _numOfStepsTaken)// && _agentManager.generationPath.Count >= (_numOfStepsTaken + 1)
+            {
+                //choose path based on probability:
+                if (ShouldFollowGenerationPathStepWithProbability(_agentManagerScript.agentProbabilityToFollowGenPath))
+                {
+                    dirTaken = _genPath[_numOfStepsTaken];//take the same direction as in the previous generation
+                    moveAndNoteVariables(dirTaken, true);
+                    if (directionDebug) Debug.Log("More directions to go GenerationPath: " + dirTaken);
+                }
+                else
+                {
+                    if (directionDebug) Debug.Log("1 More directions to go Random: " + _currentlyAvailableDirections[dirTaken]);
+                    moveAndNoteVariables(_currentlyAvailableDirections[dirTaken], true);
+                }
+            }
+            else
+            {
+                if (directionDebug) Debug.Log("2 More directions to go Random: " + _currentlyAvailableDirections[dirTaken]);
+                moveAndNoteVariables(_currentlyAvailableDirections[dirTaken], true);
+            }
+            if (directionDebug) Debug.Log("Steps taken = " + _numOfStepsTaken);
+        }
+        else if (_currentlyAvailableDirections.Count == 1)
+        {
+            if (directionDebug) Debug.Log("Just one direction to go: " + _currentlyAvailableDirections[0]);
+            moveAndNoteVariables(_currentlyAvailableDirections[0], false);
+            if (directionDebug) Debug.Log("Steps taken = " + _numOfStepsTaken);
+        }
+        else if (_currentlyAvailableDirections.Count <= 0)
+        {
+            if (directionDebug) Debug.Log("No direction to go");
+            isInDeadEnd = true;
+            _agentManagerScript.AgentAtDeadEnd(lastCrossroadPosition, pathHistory);
         }
     }
     private void UpdateAvailableDirections()
@@ -134,7 +140,7 @@ public class AgentAI : MonoBehaviour
                 _currentlyAvailableDirections.Add((int)DirectionsEnum.directions.Left);
             }
         }
-        
+
         if (_raycastScript.CanGoRight())
         {
             if (GetOppositeOfDirection(_lastDirectionTaken) != (int)DirectionsEnum.directions.Right)
@@ -142,7 +148,7 @@ public class AgentAI : MonoBehaviour
                 _currentlyAvailableDirections.Add((int)DirectionsEnum.directions.Right);
             }
         }
-        
+
         if (_raycastScript.CanGoForward())
         {
             if (GetOppositeOfDirection(_lastDirectionTaken) != (int)DirectionsEnum.directions.Forward)
@@ -150,7 +156,7 @@ public class AgentAI : MonoBehaviour
                 _currentlyAvailableDirections.Add((int)DirectionsEnum.directions.Forward);
             }
         }
-        
+
         if (_raycastScript.CanGoBack())
         {
             if (GetOppositeOfDirection(_lastDirectionTaken) != (int)DirectionsEnum.directions.Back)
@@ -160,29 +166,30 @@ public class AgentAI : MonoBehaviour
         }
 
         //print debug info
-        if(_currentlyAvailableDirections.Count == 3)
+        if (availableDirDebug)
         {
-            Debug.Log($"Currently available directions count: {_currentlyAvailableDirections.Count} >>> {_currentlyAvailableDirections[0]}, {_currentlyAvailableDirections[1]}, {_currentlyAvailableDirections[2]}");
-        }        
-        else if (_currentlyAvailableDirections.Count == 2)
-        {
-            Debug.Log($"Currently available directions count: {_currentlyAvailableDirections.Count} >>> {_currentlyAvailableDirections[0]}, {_currentlyAvailableDirections[1]}");
-        }
-        else if(_currentlyAvailableDirections.Count == 1)
-        {
-            Debug.Log($"Currently available directions count: {_currentlyAvailableDirections.Count} >>> {_currentlyAvailableDirections[0]}");
-        }
-    }
-    private void noteLastCrossroad(int pAvailableDirections)
-    {
-        if (pAvailableDirections > 1)
-        {
-            lastCrossroadPosition = this.transform.position;
+            if (_currentlyAvailableDirections.Count == 3)
+            {
+                Debug.Log($"Currently available directions count: {_currentlyAvailableDirections.Count} >>> {_currentlyAvailableDirections[0]}, {_currentlyAvailableDirections[1]}, {_currentlyAvailableDirections[2]}");
+            }
+            else if (_currentlyAvailableDirections.Count == 2)
+            {
+                Debug.Log($"Currently available directions count: {_currentlyAvailableDirections.Count} >>> {_currentlyAvailableDirections[0]}, {_currentlyAvailableDirections[1]}");
+            }
+            else if (_currentlyAvailableDirections.Count == 1)
+            {
+                Debug.Log($"Currently available directions count: {_currentlyAvailableDirections.Count} >>> {_currentlyAvailableDirections[0]}");
+            }
         }
     }
-    private void noteLastCrossroadDirectionTaken(int pDir)
+    private void moveAndNoteVariables(int pMovingInDirection, bool pNoteLastCrossroad)
     {
-        lastCrossroadDirectionTaken = pDir;
+        _numOfStepsTaken++;
+        _moveScript.MoveInDirection(pMovingInDirection);
+        _lastDirectionTaken = pMovingInDirection;
+        pathHistory.Add(pMovingInDirection);
+
+        if (pNoteLastCrossroad) lastCrossroadPosition = this.transform.position;
     }
     private int GetOppositeOfDirection(int pDir)
     {
@@ -190,16 +197,19 @@ public class AgentAI : MonoBehaviour
         if (pDir == 0)
         {
             return 1;
-        }else
+        }
+        else
         if (pDir == 1)
         {
             return 0;
-        }else
+        }
+        else
         //Forward & Back
         if (pDir == 2)
         {
             return 3;
-        }else
+        }
+        else
         if (pDir == 3)
         {
             return 2;
@@ -215,7 +225,7 @@ public class AgentAI : MonoBehaviour
     /// </summary>
     private bool ShouldFollowGenerationPathStepWithProbability(float pPercentage)
     {
-        double rnd = random.Next(0,100);
+        double rnd = AgentAI.rnd.Next(0, 100);
         if (rnd <= pPercentage)
         {
             Debug.Log("Following gen path.");
@@ -224,5 +234,25 @@ public class AgentAI : MonoBehaviour
 
         Debug.Log("NOT Following gen path.");
         return false;
+    }
+    private void initAgent()
+    {
+        if (shouldInitNewAgent)
+        {
+            if (_genPath != null)
+            {
+                _genPath.Clear();
+                _genPath = _agentManagerObj.GetComponent<AgentManager>().generationPath;
+            }
+            
+            if (pathHistory != null) pathHistory.Clear();
+
+            Debug.Assert(_genPath == null, "GenPath is null");
+            _lastDirectionTaken = -1;
+            _numOfStepsTaken = 0;
+            isInDeadEnd = false;
+
+            shouldInitNewAgent = false;
+        }
     }
 }
--- Assets/Scripts/Agents/AgentManager.cs
+++ Assets/Scripts/Agents/AgentManager.cs
@@ -1,62 +1,124 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AgentManager : MonoBehaviour
 {
+    [Tooltip("Execute step by step by pressing SPACE.")]
+    public bool stepByStep = false;
+    [Space(10)]
+    public float spawnY = 0.6f;
+    public float heightDiff = 0.3f;
+    public GameObject agentPrefab;
+    [Space(5)]
+    public bool genPathDebug = false;
+    public bool debugInfo = false;
+    [Space(5)]
     [Tooltip("Give percentage. e.g. 85, 5")]
     public float agentProbabilityToFollowGenPath = 100;
-    public int agentsInGeneration = 2;
+    public int agentsInGeneration = 3;
     public List<int> generationPath;
-    public List<GameObject> agents;
-    
+    public int generationNum;
+    [Space(10)]
+    public Text currentDataText;
+    public Text finishedText;
+
+    [SerializeField]
+    private GameObject[] agents;
     private int _agentsAtDeadEnd;
-    private Vector3 _goalPos;
+    private float _shortestDistanceToGoal;
+    private Vector3 _goalPos = Vector3.zero;
+    private Vector3 _startPos = Vector3.zero;
 
     private void Awake()
     {
-        generationPath = new List<int>() { 3, 3, 3, 1, 1, 2 };
-        foreach (GameObject agent in agents)
-        {
-            agent.GetComponent<AgentAI>().genPath.Clear();
-            agent.GetComponent<AgentAI>().genPath = generationPath;
-        }
-
-        
-
         _goalPos = GameObject.FindGameObjectWithTag("goal").transform.position;
-                Debug.Assert(_goalPos == null, "Goal not found! There is no game object with goal tag!");
-    }
-    public int GetStepInGenerationPath(int pStepNumber)
-    {
-        return generationPath[pStepNumber];
+        _startPos = GameObject.FindGameObjectWithTag("start").transform.position;
+        agents = GameObject.FindGameObjectsWithTag("agent");
+
+        initNewGenerationOfAgents(_startPos);
     }
     public void GoalFound(List<int> pPath)
     {
-        string message = "GOAL FOUND in " + pPath.Count + " steps! Steps taken: ";
-        int i = 0;
-        foreach (int dir in pPath)
+        if (generationPath != null)
         {
-            i++;
-            message += i + " = " + dir + ", ";
+            string message = "GOAL FOUND in " + pPath.Count + " steps! Steps taken: ";
+            int i = 0;
+            foreach (int dir in pPath)
+            {
+                i++;
+                message += i + " = " + dir + ", ";
+            }
+            Debug.Log(message);
         }
-        Debug.Log(message);
-    }
-    public void AgentAtDeadEnd(Vector3 pLastCrossroad, int pNumOfStepsUntilLastCrossroad, List<int> pPath)
-    {
-        //CompareCrossroadDistance()
-
-        _agentsAtDeadEnd++;
         
-        if (_agentsAtDeadEnd == agentsInGeneration)
+
+        int stepsNeeded = pPath.Count;
+
+        if(debugInfo) Debug.Log($"Goal found! {pPath.Count}, {generationNum}");
+        if (debugInfo) Debug.Log("PRESS 'R' TO RESTART");
+
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            //all agents at dead end
-            //restart generation:
-            //generationCount ++;
+            generationNum = 0;
+            stepsNeeded = 0;
+            initNewGenerationOfAgents(_startPos);
+        }
+
+        currentDataText.enabled = false;
+        finishedText.enabled = true;
+        finishedText.text = "GOAL FOUND! Generations: " + generationNum + " Agents: " + agents.Length + " R-RESTART";
+    }
+    public void AgentAtDeadEnd(Vector3 pLastCrossroad, List<int> pPath)
+    {
+        CompareCrossroadDistanceFromGoal(pLastCrossroad, pPath);
+        _agentsAtDeadEnd++;
+
+        if (_agentsAtDeadEnd == agents.Length)
+        {
+            if (debugInfo) Debug.Log("ALL AGENTS AT DEAD END: new generation started.");
+            initNewGenerationOfAgents(_startPos, pPath);
         }
     }
-    private void CompareCrossroadDistanceFromGoal()
+    private void CompareCrossroadDistanceFromGoal(Vector3 pCrossroad, List<int> pAgentPath)
     {
+        float distanceToGoal = (_goalPos - pCrossroad).magnitude;
+        if (distanceToGoal < _shortestDistanceToGoal)
+        {
+            _shortestDistanceToGoal = distanceToGoal;
+            
+            generationPath.Clear();
+            generationPath = pAgentPath;
+        }
+    }
+    private void initNewGenerationOfAgents(Vector3 pPosition, List<int> pNewGenPath = null)
+    {
+        generationPath = pNewGenPath;
+        generationNum++;
+        _agentsAtDeadEnd = 0;
 
+        foreach (GameObject agent in agents)
+        {
+            agent.transform.position = new Vector3(pPosition.x, agent.transform.position.y, pPosition.z);
+            agent.GetComponent<AgentAI>().shouldInitNewAgent = true;
+        }
+
+        finishedText.enabled = false;
+        currentDataText.enabled = true;
+        currentDataText.text = "Generation: " +generationNum+ " Agents: " +agents.Length;
+
+        if (genPathDebug)
+        {
+            if (generationPath != null)
+            {
+                string message = "Agent manager genPath: " + generationPath.Count + " steps! Steps taken: ";
+                foreach (int dir in generationPath)
+                {
+                    message += dir + ", ";
+                }
+                Debug.Log(message);
+            }
+        }
     }
 }
